a04585ac78a3507276c7c8508c399a9589a4d502
Zay <hosea8zay@gmail.com>
2022-05-31 11:29:01 -0400

    footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css"
+
+
+
+
+class Footer extends React.Component {
+
+    render() {
+        return(
+            <div className="footer-box">
+                <p className="logo-name"> Retro Perspective </p>
+                <div className="footer-icons">
+                <img src={"https://i.imgur.com/x2AIn6N.png"} alt="Logo" />
+                <img src={"https://i.imgur.com/MZopKUP.png"} alt="Logo" />
+                </div>
+                <p className="Footer-link-names">  Shows - Games  </p>
+                <p className="Footer-link-terms">  Terms of Use - Privacy Policy   </p>
+                <p className="Footer-link-date">  &copy; 2022 Retro Perspective    </p>
+            </div>
+        )
+    }
+
+
+}
+
+export default Footer;
